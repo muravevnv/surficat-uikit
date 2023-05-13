568523f36f5bc0d46a38be9f5aa385e67e6a06db
Nickolay Muravev <murvievnick@gmail.com>
2023-05-14 01:00:34 +0300

    добавил input

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const formControls = document.querySelectorAll('.js-form-control');
+
+formControls.forEach(function(item){
+  // let formInput = item.querySelector('.form__input');
+  item.addEventListener('focus',function(){
+    item.classList.add('is-focus')
+  });
+  item.addEventListener('focusout',function(){
+    item.classList.remove('is-focus')
+  });
+
+  item.addEventListener('input', function(){
+    if(item.value !== '') {
+      console.log('12123')
+      item.classList.add('is-fill');
+    } else {
+      console.log('12123')
+      item.classList.remove('is-fill');
+    }
+  })
+
+
+})
